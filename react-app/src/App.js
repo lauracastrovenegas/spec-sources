82c097e3932564e5cb29b03547ed0efeec841388
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-10-05 19:36:42 -0400

    Created react app

---
+++ react-app/src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="App">
+        <div class="Header">My Favorite Songs</div>
+        
+        {/* <!-- Input Form --> */}
+        <form class="Form">
+            <input class="form-input" type="text" id="name" placeholder="Song Title"></input>
+            <input class="form-input" type="text" id="name" placeholder="Artist"></input>
+            <button class="form-button">ADD</button>
+        </form>
+        
+       {/*  <!-- List --> */}
+        <div class="List">
+            {/* <!-- List Item 1 --> */}
+            <div class="list-item">
+                <div class='item-primary-text item-number'>1</div>
+                <div>
+                    <div class='item-primary-text'>Fireworks</div>
+                    <div class='item-secondary-text'>Katy Perry</div>
+                </div>
+                <div class='item-delete item-secondary-text'><img src="./close.png" /></div>
+            </div>
+
+            {/* <!-- List Item 2 --> */}
+            <div class="list-item">
+                <div class='item-primary-text item-number'>2</div>
+                <div>
+                    <div class='item-primary-text'>Party In The USA</div>
+                    <div class='item-secondary-text'>Miley Cyrus</div>
+                </div>
+                <div class='item-delete item-secondary-text'><img src="./close.png" /></div>
+            </div>
+
+            {/* <!-- List Item 3 --> */}
+            <div class="list-item">
+                <div class='item-primary-text item-number'>3</div>
+                <div>
+                    <div class='item-primary-text'>Dynamite</div>
+                    <div class='item-secondary-text'>Taio Cruz</div>
+                </div>
+                <div class='item-delete item-secondary-text'><img src="./close.png" /></div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
