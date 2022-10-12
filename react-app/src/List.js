ab4241175df13188325c4f7282b5a455e5cd70aa
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-10-12 16:53:44 -0400

    updated icon

--- react-app/src/List.js
+++ react-app/src/List.js
@@ -1,4 +1,6 @@
 
+import close from "./close.png"
+
 const songList = [{ songName: "Fireworks", artistName: "Katy Perry" }, { songName: "Despacito", artistName: "Luis Fonsi" }, { songName: "Fireworks", artistName: "Katy Perry" }]
 
 function List() {
@@ -22,7 +24,7 @@ function ListItem({ id, songName, artistName }) {
                 <div class='item-primary-text'>{songName}</div>
                 <div class='item-secondary-text'>{artistName}</div>
             </div>
-            <div class='item-delete item-secondary-text'><img src="./close.png" /></div>
+            <div class='item-delete item-secondary-text'><img src={close} /></div>
         </div>
     )
 }
